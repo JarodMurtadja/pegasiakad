ea30c5d20f9b6bd719ceb1a50117c5c84ff98800
JarodMurtadja <m.arhaburisqimpa@gmail.com>
2025-11-12 19:06:49 +0700

    My real PegasiAkad app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,11 +15,12 @@ class AppServiceProvider extends ServiceProvider
 {
     
     public function boot()
-    {       
-        if (env('APP_ENV') !== 'local') {
-            URL::forceScheme('https');
-        }
-
+    {   
+        {
+    if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
+}
         // Logika baru untuk role aktif
         Blade::if('role', function ($role) {
             $activeRole = session('active_role');
